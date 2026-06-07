8b7cbb3ae2db701c98983bedb6a92c9178b1e6f7
mustaphaelbaghdadime-ai <mustapha.elbaghdadi.me@gmail.com>
2026-06-07 16:40:06 -0400

    first commit

---
+++ AI.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plot
+import tensorflow as tf
+(x_train,y_train),(x_test,y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train/255.0
+x_test = x_test/255.0
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), padding="same", activation='relu', input_shape=(32,32,3)), #type: ignore
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)), #type:ignore
+    tf.keras.layers.MaxPooling2D((2,2)),
+
+    tf.keras.layers.Conv2D(32, (3,3), padding="same", activation='relu', input_shape=(32,32,3)), #type:ignore
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)), #type:ignore
+    tf.keras.layers.MaxPooling2D((2,2)),
+
+    tf.keras.layers.Conv2D(32, (3,3), padding ="same", activation='relu', input_shape=(32,32,3)), #type:ignore
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)), #type:ignore
+    tf.keras.layers.MaxPooling2D((2,2)),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(
+    optimizer='adam',
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+x_train = x_train.reshape(-1,32,32,3)
+x_test = x_test.reshape(-1,32,32,3)
+model.fit(x_train,y_train,epochs=5)
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print("Test accuracy:", test_acc)
---
+++ main.py
@@ -0,0 +1,29 @@
+import datetime
+import json
+import requests
+from playsound3 import playsound # type: ignore
+from pyttsx3 import speak
+from time import sleep
+parameters = {
+                "latitude": "45.480929",
+                "longitude": "-73.440443"}
+def set_times():
+    weird_time = datetime.datetime.now()
+    current_time = datetime.datetime.strftime(weird_time, "%d-%m-%y")
+    prayer_data = requests.get(f"https://api.aladhan.com/v1/timings/{current_time}",parameters)
+    if prayer_data.status_code != 200:
+        exit('Uh oh, the request didnt work')
+    prayer_dict: dict = json.loads(prayer_data.text)['data']['timings']
+    prayer_times: list[int] = [value for value in prayer_dict.values()]
+    return [prayer_dict,prayer_times]
+
+prayer_dict,prayer_times = set_times()
+while True:
+    time = datetime.datetime.strftime(datetime.datetime.now(),'%H:%M')
+    if time == "00:00":
+        prayer_dict, prayer_times = set_times()
+    if time in prayer_times:
+        speak(f"It is time for {prayer_dict.get(time)}")
+        playsound("Abdul-Basit.mp3")
+    sleep(60)
+
