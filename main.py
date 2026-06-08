26059845e2bbfbda4db54a0355f3ece3a2aa60ea
ilyasselbaghdadi <ilyass.elbaghdadi.5@hotmail.com>
2026-06-08 16:43:59 -0400

    Removal of playsound in favor of winsound and deletion of the calculator

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import datetime
-import json
+import datetime as dt
 import requests
-from playsound3 import playsound # type: ignore
-from pyttsx3 import speak # type: ignore
+from json import loads
 from time import sleep
-parameters = {
+from winsound import PlaySound, Beep
+parameters:dict = {
                 "latitude": "45.480929",
                 "longitude": "-73.440443"}
 def set_times() -> list:
-    weird_time = datetime.datetime.now()
-    current_time = datetime.datetime.strftime(weird_time, "%d-%m-%y")
-    prayer_data = requests.get(f"https://api.aladhan.com/v1/timings/{current_time}",parameters)
+    weird_time: dt.datetime = dt.datetime.now()
+    current_time: str = dt.datetime.strftime(weird_time, "%d-%m-%y")
+    prayer_data: requests.Response = requests.get(f"https://api.aladhan.com/v1/timings/{current_time}",parameters)
     if prayer_data.status_code != 200:
+        Beep(1000,1000)
         exit('Uh oh, the request didnt work')
-    prayer_dict: dict = json.loads(prayer_data.text)['data']['timings']
+    prayer_dict: dict = loads(prayer_data.text)['data']['timings']
+    del prayer_dict["Sunrise"]
+    del prayer_dict["Sunset"]
+    del prayer_dict["Imsak"]
+    del prayer_dict["Firstthird"]
+    del prayer_dict["Lastthird"]
     prayer_times: list[int] = [value for value in prayer_dict.values()]
     return [prayer_dict,prayer_times]
 
 prayer_dict,prayer_times = set_times()
 while True:
-    time = datetime.datetime.strftime(datetime.datetime.now(),'%H:%M')
+    time: str = dt.datetime.strftime(dt.datetime.now(),'%H:%M')
     if time == "00:00":
         prayer_dict, prayer_times = set_times()
     if time in prayer_times:
-        speak(f"It is time for {prayer_dict.get(time)}")
-        playsound("Abdul-Basit.mp3")
+        PlaySound("Abdul-Basit.wav",winsound.SND_FILENAME) # type: ignore
     sleep(60)
 
